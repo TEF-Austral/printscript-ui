5567976efb98139eb9c5981e129dc58e76ae0b7a
Franco <fdescalzo@mail.austral.edu.ar>
2025-11-07 19:26:04 -0300

    chore: update Gradle build configuration and dependencies

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const FRONTEND_URL =
-  process?.env?.FRONTEND_URL ?? "http://localhost:5173";
-export const SNIPPET_URL = process?.env?.BACKEND_URL ?? "/api/snippet";
-export const AUTH_URL = process?.env?.AUTH0_URL ?? "/api/authorization";
-export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME ?? "";
-export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD ?? "";
+    import.meta.env.VITE_FRONTEND_URL ?? "http://localhost:5173";
+export const SNIPPET_URL = import.meta.env.VITE_BACKEND_URL ?? "/api/snippet";
+export const AUTH_URL = import.meta.env.VITE_AUTH0_URL ?? "/api/authorization";
+export const AUTH0_USERNAME = import.meta.env.VITE_AUTH0_USERNAME ?? "";
+export const AUTH0_PASSWORD = import.meta.env.VITE_AUTH0_PASSWORD ?? "";
 export const PRINTSCRIPT_URL =
-    process?.env?.PRINTSCRIPT_URL ?? "/api/printscript";
+    import.meta.env.VITE_PRINTSCRIPT_URL ?? "/api/printscript";
